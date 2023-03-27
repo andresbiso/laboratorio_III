19b8154aa9811128bb013358de477d50722dd185
Andrés Biso <biso.andres@gmail.com>
2023-03-27 20:56:31 -0300

    Update productor.c

--- clase_3_tp_archivos/productor.c
+++ clase_3_tp_archivos/productor.c
@@ -3,29 +3,29 @@
 #include "defines.h"
 #include "gestionararchivos.h"
 
-int main(int argc, char *argv[]) {  int cantProd;
+int main(int argc, char *argv[]) {
+  int cantProd;
   int i;
   char nom[32+1];
   FILE *cfptr;
   memset(nom,0x00,sizeof(nom));
-  if (abrirArchivo(RUTA_ARCHIVO_PRODUCTO,"w",&cfptr))
+  if (!abrirArchivo(RUTA_ARCHIVO_PRODUCTO,"w",&cfptr))
   {
     printf("Hubo un error al querer abrir el archivo\n");
+    return 0;
   }
-  else
+  printf("Ingrese cantidad de productos:");
+  scanf("%d",&cantProd);
+  i = 1;
+  while(i <= cantProd)
   {
-    printf("Ingrese cantidad de productos:");
-    scanf("%d",&cantProd);
-    i = 1;
-    while(i <= cantProd)
-    {
-      printf("Ingrese Nombre del Producto %d:\n", i);
-      scanf("%s",nom);
-      fprintf(cfptr,"PRODUCTO-%02d %s %d\n",i,nom,UNIDADES_LOTE);
-      memset(nom,0x00,sizeof(nom));
-      i++;
-    }
-  }  cerrarArchivo(cfptr);
+    printf("Ingrese Nombre del Producto %d:\n", i);
+    scanf("%s",nom);
+    fprintf(cfptr,"PRODUCTO-%02d %s %d\n",i,nom,UNIDADES_LOTE);
+    memset(nom,0x00,sizeof(nom));
+    i++;
+  }
+  cerrarArchivo(cfptr);
   return 0;
 }
 
