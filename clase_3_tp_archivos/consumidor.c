976b3add0c08319c5756a7dcd986c34411f15ee3
Andrés Biso <biso.andres@gmail.com>
2023-03-27 21:06:02 -0300

    Update consumidor.c

--- clase_3_tp_archivos/consumidor.c
+++ clase_3_tp_archivos/consumidor.c
@@ -3,28 +3,31 @@
 #include "defines.h"
 #include "gestionararchivos.h"
 
-int main(int argc, char *argv[]) {
+void leerProductos(FILE *cfptr)
+{
   char producto[32+1];
   char nom[32+1];
   int lote;
-  FILE *cfptr;
   memset(nom,0x00,sizeof(nom));
   memset(producto,0x00,sizeof(producto));
-  if (abrirArchivo(RUTA_ARCHIVO_PRODUCTO,"r",&cfptr))
+  printf("Los productos ingresados son:\n");
+  while(!esFinArchivo(cfptr))
+  {
+    fscanf(cfptr,"%s %s %d\n",producto,nom,&lote);
+    printf("%s Nombre:%s Cantidad:%d\n",producto,nom,lote);
+    memset(nom,0x00,sizeof(nom));
+    memset(producto,0x00,sizeof(producto));
+  }
+}
+
+int main(int argc, char *argv[]) {
+  FILE *cfptr;
+  if (!abrirArchivo(RUTA_ARCHIVO_PRODUCTO,"r",&cfptr))
   {
     printf("Hubo un error al querer abrir el archivo\n");
+    return 0;
   }
-  else
-  {
-    printf("Los productos ingresados son:\n");
-    while(!esFinArchivo(cfptr))
-    {
-      fscanf(cfptr,"%s %s %d\n",producto,nom,&lote);
-      printf("%s Nombre:%s Cantidad:%d\n",producto,nom,lote);
-      memset(nom,0x00,sizeof(nom));
-      memset(producto,0x00,sizeof(producto));
-    }
-  }
+  leerProductos(cfptr);
   cerrarArchivo(cfptr);
-	return 0;
+  return 0;
 }
