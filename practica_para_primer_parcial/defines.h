f4b217e9fa043bc5d4b0989b8f4f854c81d78741
Andrés Biso <biso.andres@gmail.com>
2023-04-26 17:54:42 -0300

    add practica

---
+++ practica_para_primer_parcial/defines.h
@@ -0,0 +1,31 @@
+#ifndef _DEFINES_H_
+#define _DEFINES_H_
+#define FALSE 0
+#define TRUE 1
+#define CANTIDAD 10
+#define DESDE 0
+#define HASTA 10
+#define RUTA_ARCHIVO_FTOK "/bin/ls"
+#define CLAVE_BASE 33
+#define ROJO 0
+#define VERDE 1
+#define INTERVALO_SDC_MS 100
+#define INTERVALO_RESTO_MS 500
+#define LARGO_LINEA 100
+#define LARGO_RUTA 100
+#define RUTA_ARCHIVO_SDC "./sdc%d.txt"
+#define RUTA_ARCHIVO_MENU_A "./sdcA.txt"
+#define RUTA_ARCHIVO_MENU_B "./sdcB.txt"
+#define RUTA_ARCHIVO_MENU_C "./sdcC.txt"
+#define TIPOS_MENU_MAX 3
+#define TIPOS_MENU_MIN 1
+#define MENU_A "A"
+#define MENU_B "B"
+#define MENU_C "C"
+#define IMPORTE_MENU_A 1000
+#define IMPORTE_MENU_B 2000
+#define IMPORTE_MENU_C 3000
+#define IMPORTE_POSTRE 200
+#define FORMATO_MENU "MENU:%s\tPRECIO:%d\tTIENE_POSTRE:%d\n"
+#define FORMATO_OUTPUT "%d\t%d\t%s\n"
+#endif
---
+++ practica_para_primer_parcial/funciones.c
@@ -0,0 +1,92 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "defines.h"
+#include "funciones.h"
+
+int obtenerNumeroAleatorio(int desde, int hasta) {
+	int num = 0;
+	num = (rand()%(hasta-desde+1))+desde;
+	return num;
+}
+
+void obtenerNumerosAleatorios(int desde, int hasta, int tamanio, int* nums) {
+	int i;
+	for (i = 0; i < tamanio; i++)
+	{
+		int valor = (rand()%(hasta-desde+1))+desde;
+		nums[i] = valor;
+	}
+}
+
+void limpiarPantalla()
+{
+  int i;
+  i = 0;
+  while (i<80)
+  {
+    printf("\n");
+    i++;
+  }
+}
+
+char* obtenerRutaArchivoMenu(int valor)
+{
+  switch(valor)
+  {
+    case 1:
+      return RUTA_ARCHIVO_MENU_A;
+    case 2:
+      return RUTA_ARCHIVO_MENU_B;
+    case 3:
+      return RUTA_ARCHIVO_MENU_C;
+    default:
+      return "";
+  }
+}
+
+char* obtenerMenuLetra(int valor)
+{
+  switch(valor)
+  {
+    case 1:
+      return MENU_A;
+    case 2:
+      return MENU_B;
+    case 3:
+      return MENU_C;
+  }
+  return 0;
+}
+
+int obtenerMenuNum(char* valor)
+{
+  printf("%s", valor);
+  if (strcmp("A", valor) == 0)
+  {
+    return 1;
+  }
+  else if (strcmp("B", valor) == 0)
+  {
+    return 2;
+  }
+  else if (strcmp("C", valor) == 0)
+  {
+    return 3;
+  }
+  return -1;
+}
+
+int obtenerPrecioMenu(int valor)
+{
+  switch(valor)
+  {
+    case 1:
+      return IMPORTE_MENU_A;
+    case 2:
+      return IMPORTE_MENU_B;
+    case 3:
+      return IMPORTE_MENU_C;
+  }
+  return 0;
+}
